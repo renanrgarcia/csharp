e5c697a575e2d76ffe1b3a71be3dd5462e6e71e2
Renan Garcia <renanrgarcia@gmail.com>
2022-11-01 12:38:13 -0300

    Desafio8 adicionado

---
+++ Desafios_11/Desafio8/Desafio8/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+// Crie um algoritmo que após receber o Salário Bruto, calcule o salário líquido descontando 25% quando
+// o salário bruto for menor que R$ 2000, caso contrário descontar 35%. No final, exiba o salário líquido.
+
+namespace Desafio8 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Esse programa calcula o salário líquido dado desconto ao salário bruto.");
+            Console.Write("Digite o salário bruto: R$ ");
+            double salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double salarioliquido = CalculadoraSalario.SalarioLiquido(salario);
+
+            Console.WriteLine("O salário líquido é R$ " + salarioliquido.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
