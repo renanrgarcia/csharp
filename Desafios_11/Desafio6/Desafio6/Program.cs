3fe05e0f23b91a2c89592efcf8a8255347283629
Renan Garcia <renanrgarcia@gmail.com>
2022-11-01 10:09:02 -0300

    Desafio6 adicionado

---
+++ Desafios_11/Desafio6/Desafio6/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+// Crie um algoritmo que leia um número inteiro e informe se ele é par ou ímpar.
+
+namespace Desafio6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Esse programa informa se o número digitado é par ou ímpar.");
+            Console.Write("Digite um número inteiro: ");
+            int numero = int.Parse(Console.ReadLine());
+
+            if (numero % 2 == 0)
+                Console.WriteLine("O número {0} é par.", numero);
+            else
+                Console.WriteLine("O número {0} é ímpar.", numero);
+        }
+    }
+}
+
