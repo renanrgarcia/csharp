c2efa669c2e1e4057b2b6a3853d5d35c64fde3ab
Renan Garcia <renanrgarcia@gmail.com>
2022-10-31 22:57:24 -0300

    Desafio1 incluído

---
+++ Desafios_11/Desafio1/Desafio1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+
+// Crie um algoritmo que leia um número e alerte quando o mesmo for maior do que 500.
+
+namespace Desafio1 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Esse programa responde se o número digitado é maior do que 500.");
+            Console.Write("Digite um número: ");
+            double n = double.Parse(Console.ReadLine().ToString(CultureInfo.InvariantCulture));
+
+            if (n == 500.0)
+                Console.WriteLine("O número informado é igual a 500.");
+            else if (n > 500.0)
+                Console.WriteLine("O número informado é maior que 500.");
+            else
+                Console.WriteLine("O número informado é menor que 500.");
+
+        }
+    }
+}
