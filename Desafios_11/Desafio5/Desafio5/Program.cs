a004ecd39b5cb7298d2dce9e55a338d4a798e661
Renan Garcia <renanrgarcia@gmail.com>
2022-11-01 09:57:25 -0300

    Desafio5 adicionado

---
+++ Desafios_11/Desafio5/Desafio5/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+// Crie um algoritmo que leia um número inteiro e informe se ele é ou não divisível por 7.
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+                Console.WriteLine("Esse programa informa se o número digitado é divisível por 7.");
+                Console.Write("Digite um número inteiro: ");
+                int numero = int.Parse(Console.ReadLine());
+
+                if (numero % 7 == 0)
+                    Console.WriteLine("O número " + numero + " é divisível por 7.");
+                else
+                    Console.WriteLine("O número " + numero + " não é divisível por 7.");
+
+        }
+    }
+}
