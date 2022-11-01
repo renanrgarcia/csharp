bd2388adf99efe61c8a9b8d3d4d1e324d51d3f01
Renan Garcia <renanrgarcia@gmail.com>
2022-11-01 14:26:03 -0300

    Desafio9 adicionado

---
+++ Desafios_11/Desafio9/Desafio9/Produto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+
+namespace Desafio9
+{
+    internal class Produto
+    {
+        public double PrecoCompra { get; set; }
+
+        public Produto(double precoCompra)
+        {
+            PrecoCompra = precoCompra;
+        }
+
+        public static double Precificacao(double precoCompra)
+        {
+            if (precoCompra >= 200)
+                return precoCompra += precoCompra * 0.315;
+            else
+                return precoCompra -= precoCompra * 0.5;
+        }
+
+        public override string ToString()
+        {
+            return "O preço de compra é R$ " + PrecoCompra.ToString("F2", CultureInfo.InvariantCulture) + ". O preço de venda é de R$ " 
+                + Precificacao(PrecoCompra).ToString("F2", CultureInfo.InvariantCulture) + ".";
+        }
+    }
+}
---
+++ Desafios_11/Desafio9/Desafio9/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+// Um comerciante comprou um produto e quer vendê-lo com um lucro de 50% se o valor da compra for menor que R$ 200,
+// caso contrário o lucro será de 31,5%. Crie um algoritmo que leia o valor da compra e exiba o valor da venda.
+
+namespace Desafio9 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Esse programa retorna o valor de venda de um produto e o lucro obtido.");
+            Console.Write("Digite o valor de compra do produto: ");
+            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Produto product = new Produto(preco);
+
+            Console.WriteLine(product);
+        }
+    }
+}
+
+
