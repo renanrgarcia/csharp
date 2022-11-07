58f28f834b3505b50cb59ae2f695dee9dba4e7d6
Renan Garcia <renanrgarcia@gmail.com>
2022-11-06 22:46:56 -0300

    Desafio2 - Adicionada classe Login

--- Desafios_11/Desafio2/Desafio2/Program.cs
+++ Desafios_11/Desafio2/Desafio2/Program.cs
@@ -4,7 +4,7 @@ using System.Net.Mail;
 // Crie um algoritmo que leia uma senha fornecida pelo usuário composta por caracteres para informar se a senha é válida ou inválida.
 // A senha do sistema corresponde a palavra “SENHA”. Obs.: O algoritmo não faz distinção de letras maiúsculas e minúsculas.
 
-namespace Desafio2 // Note: actual namespace depends on the project name.
+namespace Desafio2 
 {
     internal class Program
     {
@@ -12,13 +12,18 @@ namespace Desafio2 // Note: actual namespace depends on the project name.
         {
             Console.WriteLine("Esse sistema informa se a senha informada é válida ou não.");
             Console.Write("Digite sua senha: ");
-            string password = Console.ReadLine();
-            password = password.ToUpper();
+            string senha = Console.ReadLine();
+            Login log = new Login(senha);
 
-            if (password == "SENHA")
+            if (log.TesteSenha(senha) == true)
+            {
                 Console.WriteLine("Senha válida!");
+            }
             else
+            {
                 Console.WriteLine("Senha inválida!");
+            }
+            
 
         }
     }
