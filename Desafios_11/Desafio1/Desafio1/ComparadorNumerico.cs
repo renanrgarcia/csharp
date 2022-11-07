0c324b92c6079b310c22682330cc83d7a5832fb2
Renan Garcia <renanrgarcia@gmail.com>
2022-11-06 22:36:26 -0300

    Desafio1 - Adicionado try-catch e classe ComparadorNumerico

---
+++ Desafios_11/Desafio1/Desafio1/ComparadorNumerico.cs
@@ -0,0 +1,22 @@
+﻿namespace Desafio1
+{
+    internal class ComparadorNumerico
+    {
+        public double Numero { get; set; }
+
+        public ComparadorNumerico (double numero)
+        {
+            Numero = numero;
+        }
+
+        public string ComparaNumero(double numero)
+        {
+            if (numero == 500.0)
+                return "O número informado é igual a 500.";
+            else if (numero > 500.0)
+                return "O número informado é maior que 500.";
+            else
+                return "O número informado é menor que 500.";
+        }
+    }
+}
--- Desafios_11/Desafio1/Desafio1/Program.cs
+++ Desafios_11/Desafio1/Desafio1/Program.cs
@@ -3,24 +3,25 @@ using System.Globalization;
 
 // Crie um algoritmo que leia um número e alerte quando o mesmo for maior do que 500.
 
-namespace Desafio1 
+namespace Desafio1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            try
+            {
+                Console.WriteLine("Esse programa responde se o número digitado é maior do que 500.");
+                Console.Write("Digite um número: ");
+                double numero = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-            Console.WriteLine("Esse programa responde se o número digitado é maior do que 500.");
-            Console.Write("Digite um número: ");
-            double n = double.Parse(Console.ReadLine().ToString(CultureInfo.InvariantCulture));
-
-            if (n == 500.0)
-                Console.WriteLine("O número informado é igual a 500.");
-            else if (n > 500.0)
-                Console.WriteLine("O número informado é maior que 500.");
-            else
-                Console.WriteLine("O número informado é menor que 500.");
-
+                ComparadorNumerico comp = new ComparadorNumerico(numero);
+                Console.WriteLine(comp.ComparaNumero(numero));
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
