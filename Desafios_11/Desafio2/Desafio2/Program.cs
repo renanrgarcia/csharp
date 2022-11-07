98b9cc27a4c389e499c7c4c700d081ea440ddbdd
Renan Garcia <renanrgarcia@gmail.com>
2022-11-07 18:53:25 -0300

    Desafio2 - Adicionado while para reutilização do programa e try-catch

--- Desafios_11/Desafio2/Desafio2/Program.cs
+++ Desafios_11/Desafio2/Desafio2/Program.cs
@@ -4,27 +4,40 @@ using System.Net.Mail;
 // Crie um algoritmo que leia uma senha fornecida pelo usuário composta por caracteres para informar se a senha é válida ou inválida.
 // A senha do sistema corresponde a palavra “SENHA”. Obs.: O algoritmo não faz distinção de letras maiúsculas e minúsculas.
 
-namespace Desafio2 
+namespace Desafio2
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Esse sistema informa se a senha informada é válida ou não.");
-            Console.Write("Digite sua senha: ");
-            string senha = Console.ReadLine();
-            Login log = new Login(senha);
-
-            if (log.TesteSenha(senha) == true)
+            try
             {
-                Console.WriteLine("Senha válida!");
-            }
-            else
-            {
-                Console.WriteLine("Senha inválida!");
-            }
-            
+                char resposta = 's';
+                while (resposta != 'n')
+                {
+                    Console.WriteLine("Esse sistema informa se a senha informada é válida ou não.");
+                    Console.Write("Digite sua senha: ");
+                    string senha = Console.ReadLine();
+                    Login log = new Login(senha);
 
+                    if (log.TesteSenha(senha) == true)
+                    {
+                        Console.WriteLine("Senha válida!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Senha inválida!");
+                    }
+                    Console.Write("Gostaria de eecutar o programa novamente (s/n)? ");
+                    resposta = char.Parse(Console.ReadLine());
+                    Console.Clear();
+                }
+                Console.WriteLine("Obrigado por utilizar o programa!");
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
