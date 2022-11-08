d570cb8cf3b35b61653469f01c30df44f5e56356
Renan Garcia <renanrgarcia@gmail.com>
2022-11-08 15:04:43 -0300

    Desafio9 - Simplificado

--- Desafios_11/Desafio9/Desafio9/Program.cs
+++ Desafios_11/Desafio9/Desafio9/Program.cs
@@ -4,19 +4,49 @@ using System.Globalization;
 // Um comerciante comprou um produto e quer vendê-lo com um lucro de 50% se o valor da compra for menor que R$ 200,
 // caso contrário o lucro será de 31,5%. Crie um algoritmo que leia o valor da compra e exiba o valor da venda.
 
-namespace Desafio9 
+namespace Desafio9
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Esse programa retorna o valor de venda de um produto e o lucro obtido.");
-            Console.Write("Digite o valor de compra do produto: ");
-            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            try
+            {
+                char resposta = 's';
+                while (resposta != 'n')
+                {
+                    Console.WriteLine("Esse programa retorna o valor de venda de um produto e o lucro obtido.");
+                    Console.Write("Digite o valor de compra do produto: ");
+                    double precoCompra = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    double precoVenda = 0.0;
+                    double lucro = 0.0;
 
-            Produto product = new Produto(preco);
+                    if (precoCompra >= 200)
+                    {
+                        lucro = 0.315;
+                        precoVenda = precoCompra * (lucro + 1);
+                    }
+                    else
+                    {
+                        lucro = 0.5;
+                        precoVenda = precoCompra * (lucro + 1);
+                    }
 
-            Console.WriteLine(product);
+                    double porcentagemLucro = lucro * 100;
+
+                    Console.WriteLine("O preço de venda é R$ " + precoVenda.ToString("F2", CultureInfo.InvariantCulture)
+                        + ". A porcentagem de lucro foi: " + porcentagemLucro.ToString("F1", CultureInfo.InvariantCulture) + "%.");
+
+                    Console.Write("Gostaria de executar o programa novamente (s/n)? ");
+                    resposta = char.Parse(Console.ReadLine());
+                    Console.Clear();
+                }
+                Console.WriteLine("Obrigado por utilizar o programa!");
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
