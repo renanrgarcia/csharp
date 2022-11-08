31835b4c97ba2c9735f7b8bb075cb210b01167c6
Renan Garcia <renanrgarcia@gmail.com>
2022-11-08 14:28:52 -0300

    Desafio5 - Simplificado

--- Desafios_11/Desafio5/Desafio5/Program.cs
+++ Desafios_11/Desafio5/Desafio5/Program.cs
@@ -8,16 +8,30 @@ namespace MyApp // Note: actual namespace depends on the project name.
     {
         static void Main(string[] args)
         {
+            try
+            {
+                char resposta = 's';
+                while (resposta != 'n')
+                {
+                    Console.WriteLine("Esse programa informa se o número digitado é divisível por 7.");
+                    Console.Write("Digite um número inteiro: ");
+                    int numero = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("Esse programa informa se o número digitado é divisível por 7.");
-            Console.Write("Digite um número inteiro: ");
-            int numero = int.Parse(Console.ReadLine());
+                    if (numero % 7 == 0)
+                        Console.WriteLine("O número " + numero + " é divisível por 7. Resto: 0");
+                    else
+                        Console.WriteLine("O número " + numero + " não é divisível por 7. Resto: " + numero % 7);
 
-            if (numero % 7 == 0)
-                Console.WriteLine("O número " + numero + " é divisível por 7.");
-            else
-                Console.WriteLine("O número " + numero + " não é divisível por 7.");
-
+                    Console.Write("Gostaria de executar o programa novamente (s/n)? ");
+                    resposta = char.Parse(Console.ReadLine());
+                    Console.Clear();
+                }
+                Console.WriteLine("Obrigado por utilizar o programa!");
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
