406dfcca3647b07540530f6e0eabbf1e7fd491e0
Renan Garcia <renanrgarcia@gmail.com>
2022-11-08 15:41:58 -0300

    Desafio11 - Simplificado

--- Desafios_11/Desafio11/Desafio11/Program.cs
+++ Desafios_11/Desafio11/Desafio11/Program.cs
@@ -10,77 +10,137 @@ namespace Desafio11
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Esse programa exibe os três números digitados em ordem crescente.");
-            Console.Write("Digite os três números entre espaços: ");
-            string[] s = Console.ReadLine().Split(' ');
-            double n1 = double.Parse(s[0], CultureInfo.InvariantCulture);
-            double n2 = double.Parse(s[1], CultureInfo.InvariantCulture);
-            double n3 = double.Parse(s[2], CultureInfo.InvariantCulture);
-
-            ComparaNumeros comp = new ComparaNumeros(n1, n2, n3);
-
-            // 3 números iguais
-            if (n1 == n2 && n2 == n3)
-                Console.WriteLine("Os três números são iguais! Seguem os números: "
-                    + n1.ToString("F2", CultureInfo.InvariantCulture) + ", "
-                    + n2.ToString("F2", CultureInfo.InvariantCulture) + ", "
-                    + n3.ToString("F2", CultureInfo.InvariantCulture) + ".");
-
-            // 2 números iguais
-            else if (n1 == n2 || n2 == n3 || n1 == n3)
+            try
             {
-                double maiorDoisIguais = comp.MaiorDoisIguais(n1, n2, n3);
-                double menorDoisIguais = comp.MenorDoisIguais(n1, n2, n3);
-                Console.WriteLine("Há dois números iguais! Segue a ordem crescente: ");
+                char resposta = 's';
+                while (resposta != 'n')
+                {
+                    Console.WriteLine("Esse programa exibe os três números digitados em ordem crescente.");
+                    Console.Write("Digite os três números entre espaços: ");
+                    string[] s = Console.ReadLine().Split(' ');
+                    double n1 = double.Parse(s[0], CultureInfo.InvariantCulture);
+                    double n2 = double.Parse(s[1], CultureInfo.InvariantCulture);
+                    double n3 = double.Parse(s[2], CultureInfo.InvariantCulture);
 
-                double[] num = new double[3];
-                num[0] = n1;
-                num[1] = n2;
-                num[2] = n3;
+                    // 3 números iguais
+                    if (n1 == n2 && n2 == n3)
+                        Console.WriteLine("Os três números são iguais! Seguem os números: "
+                            + n1.ToString(CultureInfo.InvariantCulture) + ", "
+                            + n2.ToString(CultureInfo.InvariantCulture) + ", "
+                            + n3.ToString(CultureInfo.InvariantCulture) + ".");
 
-                // Dois iguais maiores
-                if (maiorDoisIguais == n1 && n1 == n2)
-                    Console.Write("{0}, {1}, {2}.", n3.ToString(CultureInfo.InvariantCulture),
-                        n1.ToString(CultureInfo.InvariantCulture),
-                        n2.ToString(CultureInfo.InvariantCulture));
-                else if (maiorDoisIguais == n2 && n2 == n3)
-                    Console.Write("{0}, {1}, {2}.", n1.ToString(CultureInfo.InvariantCulture),
-                        n2.ToString(CultureInfo.InvariantCulture),
-                        n3.ToString(CultureInfo.InvariantCulture));
-                else if (maiorDoisIguais == n3 && n3 == n1)
-                    Console.Write("{0}, {1}, {2}.", n2.ToString(CultureInfo.InvariantCulture),
-                        n3.ToString(CultureInfo.InvariantCulture),
-                        n1.ToString(CultureInfo.InvariantCulture));
-                // Dois iguais menores
-                else if (menorDoisIguais == n1 && n1 == n2)
-                    Console.Write("{0}, {1}, {2}.", n1.ToString(CultureInfo.InvariantCulture),
-                        n2.ToString(CultureInfo.InvariantCulture),
-                        n3.ToString(CultureInfo.InvariantCulture));
-                else if (menorDoisIguais == n2 && n2 == n3)
-                    Console.Write("{0}, {1}, {2}.", n2.ToString(CultureInfo.InvariantCulture),
-                        n3.ToString(CultureInfo.InvariantCulture),
-                        n1.ToString(CultureInfo.InvariantCulture));
+                    // 2 números iguais
+                    else if (n1 == n2 || n2 == n3 || n1 == n3)
+                    {
+                        double maiorDoisIguais = MaiorDoisIguais(n1, n2, n3);
+                        double menorDoisIguais = MenorDoisIguais(n1, n2, n3);
+                        Console.Write("Há dois números iguais! Segue a ordem crescente: ");
+
+                        double[] num = new double[3];
+                        num[0] = n1;
+                        num[1] = n2;
+                        num[2] = n3;
+
+                        // Dois iguais maiores
+                        if (maiorDoisIguais == n1 && n1 == n2)
+                            Console.WriteLine("{0}, {1}, {2}.", n3.ToString(CultureInfo.InvariantCulture),
+                                n1.ToString(CultureInfo.InvariantCulture),
+                                n2.ToString(CultureInfo.InvariantCulture));
+                        else if (maiorDoisIguais == n2 && n2 == n3)
+                            Console.WriteLine("{0}, {1}, {2}.", n1.ToString(CultureInfo.InvariantCulture),
+                                n2.ToString(CultureInfo.InvariantCulture),
+                                n3.ToString(CultureInfo.InvariantCulture));
+                        else if (maiorDoisIguais == n3 && n3 == n1)
+                            Console.WriteLine("{0}, {1}, {2}.", n2.ToString(CultureInfo.InvariantCulture),
+                                n3.ToString(CultureInfo.InvariantCulture),
+                                n1.ToString(CultureInfo.InvariantCulture));
+                        // Dois iguais menores
+                        else if (menorDoisIguais == n1 && n1 == n2)
+                            Console.WriteLine("{0}, {1}, {2}.", n1.ToString(CultureInfo.InvariantCulture),
+                                n2.ToString(CultureInfo.InvariantCulture),
+                                n3.ToString(CultureInfo.InvariantCulture));
+                        else if (menorDoisIguais == n2 && n2 == n3)
+                            Console.WriteLine("{0}, {1}, {2}.", n2.ToString(CultureInfo.InvariantCulture),
+                                n3.ToString(CultureInfo.InvariantCulture),
+                                n1.ToString(CultureInfo.InvariantCulture));
+                        else
+                            Console.WriteLine("{0}, {1}, {2}.", n3.ToString(CultureInfo.InvariantCulture),
+                                n1.ToString(CultureInfo.InvariantCulture),
+                                n2.ToString(CultureInfo.InvariantCulture));
+                    }
+
+                    // 3 números diferentes
+                    else // if (n1 != n2 && n2 != n3)
+                    {
+                        double maior = MaiorNumero(n1, n2, n3);
+                        double menor = MenorNumero(n1, n2, n3);
+                        double central = NumeroCentral(n1, n2, n3);
+                        Console.WriteLine("Os números em ordem crescente são "
+                            + menor.ToString(CultureInfo.InvariantCulture) + ", "
+                            + central.ToString(CultureInfo.InvariantCulture) + ", "
+                            + maior.ToString(CultureInfo.InvariantCulture) + ".");
+                    }
+
+                    Console.Write("Gostaria de executar o programa novamente (s/n)? ");
+                    resposta = char.Parse(Console.ReadLine());
+                    Console.Clear();
+                }
+                Console.WriteLine("Obrigado por utilizar o programa!");
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+        }
+
+        public static double MaiorNumero(double n1, double n2, double n3)
+        {
+            return Math.Max(n1, Math.Max(n2, n3));
+        }
+
+        public static double MenorNumero(double n1, double n2, double n3)
+        {
+            return Math.Min(n1, Math.Min(n2, n3));
+        }
+
+        public static double NumeroCentral(double n1, double n2, double n3)
+        {
+            if (MaiorNumero(n1, n2, n3) == n1)
+                if (MenorNumero(n1, n2, n3) == n2)
+                    return n3;
                 else
-                    Console.Write("{0}, {1}, {2}.", n3.ToString(CultureInfo.InvariantCulture),
-                        n1.ToString(CultureInfo.InvariantCulture),
-                        n2.ToString(CultureInfo.InvariantCulture));
-            }
+                    return n2;
+            if (MaiorNumero(n1, n2, n3) == n2)
+                if (MenorNumero(n1, n2, n3) == n1)
+                    return n3;
+                else
+                    return n1;
+            else
+                if (MenorNumero(n1, n2, n3) == n1)
+                return n2;
+            else
+                return n1;
+        }
 
-            // 3 números diferentes
-            else // if (n1 != n2 && n2 != n3)
-            {
-                double maior = comp.MaiorNumero(n1, n2, n3);
-                double menor = comp.MenorNumero(n1, n2, n3);
-                double central = comp.NumeroCentral(n1, n2, n3);
-                Console.WriteLine("Os números em ordem crescente são "
-                    + menor.ToString("F2", CultureInfo.InvariantCulture) + ", "
-                    + central.ToString("F2", CultureInfo.InvariantCulture) + ", "
-                    + maior.ToString("F2", CultureInfo.InvariantCulture) + ".");
+        public static double MaiorDoisIguais(double n1, double n2, double n3)
+        {
+            if (n1 == n2)
+                return Math.Max(n2, n3);
+            else if (n2 == n3)
+                return Math.Max(n3, n1);
+            else
+                return Math.Max(n1, n2);
+        }
 
-            }
-
-
-
+        public static double MenorDoisIguais(double n1, double n2, double n3)
+        {
+            if (n1 == n2)
+                return Math.Min(n2, n3);
+            else if (n2 == n3)
+                return Math.Min(n3, n1);
+            else
+                return Math.Min(n1, n2);
         }
     }
 }
