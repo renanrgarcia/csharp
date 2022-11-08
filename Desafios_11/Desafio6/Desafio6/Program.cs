ca6113ba0c64faeec506b6b567e38a9837e6168a
Renan Garcia <renanrgarcia@gmail.com>
2022-11-08 14:33:16 -0300

    Desafio6 - Simplificado

--- Desafios_11/Desafio6/Desafio6/Program.cs
+++ Desafios_11/Desafio6/Desafio6/Program.cs
@@ -8,14 +8,30 @@ namespace Desafio6
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Esse programa informa se o número digitado é par ou ímpar.");
-            Console.Write("Digite um número inteiro: ");
-            int numero = int.Parse(Console.ReadLine());
+            try
+            {
+                char resposta = 's';
+                while (resposta != 'n')
+                {
+                    Console.WriteLine("Esse programa informa se o número digitado é par ou ímpar.");
+                    Console.Write("Digite um número inteiro: ");
+                    int numero = int.Parse(Console.ReadLine());
 
-            if (numero % 2 == 0)
-                Console.WriteLine("O número {0} é par.", numero);
-            else
-                Console.WriteLine("O número {0} é ímpar.", numero);
+                    if (numero % 2 == 0)
+                        Console.WriteLine("O número {0} é par.", numero);
+                    else
+                        Console.WriteLine("O número {0} é ímpar.", numero);
+
+                    Console.Write("Gostaria de executar o programa novamente (s/n)? ");
+                    resposta = char.Parse(Console.ReadLine());
+                    Console.Clear();
+                }
+                Console.WriteLine("Obrigado por utilizar o programa!");
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
