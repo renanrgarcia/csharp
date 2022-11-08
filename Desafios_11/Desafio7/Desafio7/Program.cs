1f164c7028455758f054f122ae0c4f7daa2cf35a
Renan Garcia <renanrgarcia@gmail.com>
2022-11-08 14:41:08 -0300

    Desafio7 - Simplificado

--- Desafios_11/Desafio7/Desafio7/Program.cs
+++ Desafios_11/Desafio7/Desafio7/Program.cs
@@ -3,26 +3,40 @@ using System.Globalization;
 
 // Crie um algoritmo que leia dois valores diferentes e determine o maior.
 
-
 namespace Desafio7
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Esse programa determina o maior entre dois valores diferentes.");
-            Console.Write("Informe os dois valores com espaço entre eles: ");
-            string[] s = Console.ReadLine().Split(' ');
-            double numero1 = double.Parse(s[0], CultureInfo.InvariantCulture);
-            double numero2 = double.Parse(s[1], CultureInfo.InvariantCulture);
+            try
+            {
+                char resposta = 's';
+                while (resposta != 'n')
+                {
+                    Console.WriteLine("Esse programa determina o maior entre dois valores diferentes.");
+                    Console.Write("Informe os dois valores com espaço entre eles: ");
+                    string[] s = Console.ReadLine().Split(' ');
+                    double numero1 = double.Parse(s[0], CultureInfo.InvariantCulture);
+                    double numero2 = double.Parse(s[1], CultureInfo.InvariantCulture);
 
-            if (numero1 == numero2)
-                Console.WriteLine($"Os números {numero1.ToString(CultureInfo.InvariantCulture)} e {numero2.ToString(CultureInfo.InvariantCulture)} são iguais.");
-            else if (numero1 > numero2)
-                Console.WriteLine($"O número {numero1.ToString(CultureInfo.InvariantCulture)} é maior que {numero2.ToString(CultureInfo.InvariantCulture)}.");
-            else 
-                Console.WriteLine($"O número {numero2.ToString(CultureInfo.InvariantCulture)} é maior que {numero1.ToString(CultureInfo.InvariantCulture)}.");
+                    if (numero1 == numero2)
+                        Console.WriteLine($"Os números {numero1.ToString(CultureInfo.InvariantCulture)} e {numero2.ToString(CultureInfo.InvariantCulture)} são iguais.");
+                    else if (numero1 > numero2)
+                        Console.WriteLine($"O número {numero1.ToString(CultureInfo.InvariantCulture)} é maior que {numero2.ToString(CultureInfo.InvariantCulture)}.");
+                    else
+                        Console.WriteLine($"O número {numero2.ToString(CultureInfo.InvariantCulture)} é maior que {numero1.ToString(CultureInfo.InvariantCulture)}.");
 
+                    Console.Write("Gostaria de executar o programa novamente (s/n)? ");
+                    resposta = char.Parse(Console.ReadLine());
+                    Console.Clear();
+                }
+                Console.WriteLine("Obrigado por utilizar o programa!");
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
