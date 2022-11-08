e98c2bbe1c665b2de5ce1dd5c00f154d99286172
Renan Garcia <renanrgarcia@gmail.com>
2022-11-08 07:45:58 -0300

    Desafio3 - Incluida classe, try-catch e iteração

--- Desafios_11/Desafio3/Desafio3/Program.cs
+++ Desafios_11/Desafio3/Desafio3/Program.cs
@@ -3,21 +3,40 @@ using System.Globalization;
 
 // Crie um algoritmo que leia um número e alerte quando o mesmo estiver entre -100 e 100.
 
-namespace Desafio3 
+namespace Desafio3
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            
-            Console.WriteLine("Esse sistema alerta se o número informado está entre -100 e 100.");
-            Console.Write("Digite um número: ");
-            double n = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            try
+            {
+                char resposta = 's';
+                while (resposta != 'n')
+                {
+                    Console.WriteLine("Esse sistema alerta se o número informado está entre -100 e 100.");
+                    Console.Write("Digite um número: ");
+                    double x = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    ComparaNumero numero = new ComparaNumero(x);
 
-            if (n >= -100.0 && n <= 100.0)
-                Console.WriteLine($"O número é {n} e está entre -100 e 100.");
-            else
-                Console.WriteLine($"O número é {n} não está entre -100 e 100.");
+                    if (numero.PertenceIntervalo(x) == true)
+                    {
+                        Console.WriteLine("O número é " + x.ToString(CultureInfo.InvariantCulture) + " e está entre -100 e 100.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("O número é " + x.ToString(CultureInfo.InvariantCulture) + " e não está entre -100 e 100.");
+                    }
+                    Console.Write("Gostaria de eecutar o programa novamente (s/n)? ");
+                    resposta = char.Parse(Console.ReadLine());
+                    Console.Clear();
+                }
+                Console.WriteLine("Obrigado por utilizar o programa!");
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine(e.Message);
+            }
 
         }
     }
