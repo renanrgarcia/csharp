2e76a5747dfd06860adbe511365d9bbc6262b9ea
Renan Garcia <renanrgarcia@gmail.com>
2022-11-10 14:58:30 -0300

    Desafios 1 e 2 - atualizacoes com Jucinei

--- Desafios_11/Desafio1/Desafio1/Program.cs
+++ Desafios_11/Desafio1/Desafio1/Program.cs
@@ -12,22 +12,25 @@ namespace Desafio1
             try
             {
                 char resposta = 's';
+                double constante = 100.0;
                 while (resposta != 'n')
                 {
-                    Console.WriteLine("Esse programa responde se o número digitado é maior do que 500.");
+                    Console.WriteLine($"Esse programa responde se o número digitado é maior do que {constante}.");
                     Console.Write("Digite um número: ");
                     double numero = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-                    if (numero == 500.0)
-                        Console.WriteLine("O número informado é igual a 500.");
-                    else if (numero > 500.0)
-                        Console.WriteLine("O número informado é maior que 500.");
-                    else
-                        Console.WriteLine("O número informado é menor que 500.");
+                    if (numero == constante)
+                        Console.WriteLine($"O número informado é igual a {constante}.");
+                    else if (numero > constante)
+                        Console.WriteLine($"O número informado é maior que {constante}.");
+                    else 
+                        Console.WriteLine($"O número informado é menor que {constante}.");
 
                     Console.Write("Gostaria de executar o programa novamente (s/n)? ");
-                    resposta = char.Parse(Console.ReadLine());
+                    resposta = char.Parse(Console.ReadLine().ToLower());
                     Console.Clear();
+
+                    constante *= 2;
                 }
                 Console.WriteLine("Obrigado por utilizar o programa!");
             }
--- Desafios_11/Desafio2/Desafio2/Program.cs
+++ Desafios_11/Desafio2/Desafio2/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.Net.Mail;
+using System.Globalization;
 
 // Crie um algoritmo que leia uma senha fornecida pelo usuário composta por caracteres para informar se a senha é válida ou inválida.
 // A senha do sistema corresponde a palavra “SENHA”. Obs.: O algoritmo não faz distinção de letras maiúsculas e minúsculas.
@@ -12,21 +12,27 @@ namespace Desafio2
         {
             try
             {
-                char resposta = 's';
-                while (resposta != 'n')
+                string resposta = "s";
+                while (resposta != "n")
                 {
                     Console.WriteLine("Esse sistema informa se a senha informada é válida ou não.");
                     Console.Write("Digite sua senha: ");
-                    string senha = Console.ReadLine();
-                    
-                    senha = senha.ToUpper();
-                    if (senha == "SENHA")
-                        Console.WriteLine("Senha válida!");
+                    string senha = Console.ReadLine().ToUpper();
+
+                    if (senha.Equals("SENHA"))
+                        Console.WriteLine("Senha válida!"); 
                     else
                         Console.WriteLine("Senha inválida!");
 
                     Console.Write("Gostaria de executar o programa novamente (s/n)? ");
-                    resposta = char.Parse(Console.ReadLine());
+                    resposta = Console.ReadLine().ToLower();
+                    while (resposta.Length > 1)
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Você digitou mais de um caracter. Digite (s) para sim ou (n) para não.");
+                        Console.Write("Gostaria de executar o programa novamente (s/n)? ");
+                        resposta = Console.ReadLine().ToLower();
+                    }
                     Console.Clear();
                 }
                 Console.WriteLine("Obrigado por utilizar o programa!");
