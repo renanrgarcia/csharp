a6716402de98ef7916b504bb3a3b1092d6e4fc2b
Renan Garcia <renanrgarcia@gmail.com>
2022-11-12 23:04:47 -0300

    Desafio 6 - Ajuste reutilização do sistema + TryParse

--- Desafios_11/Desafio6/Desafio6/Program.cs
+++ Desafios_11/Desafio6/Desafio6/Program.cs
@@ -10,20 +10,39 @@ namespace Desafio6
         {
             try
             {
-                char resposta = 's';
-                while (resposta != 'n')
+                string resposta = "s";
+                while (resposta != "n")
                 {
                     Console.WriteLine("Esse programa informa se o número digitado é par ou ímpar.");
                     Console.Write("Digite um número inteiro: ");
-                    int numero = int.Parse(Console.ReadLine());
+                    string valor = Console.ReadLine();
+                    int numero;
 
-                    if (numero % 2 == 0)
-                        Console.WriteLine("O número {0} é par.", numero);
+                    bool successo = int.TryParse(valor, out numero);
+                    if (successo)
+                    {
+                        if (numero % 2 == 0)
+                            Console.WriteLine($"O número {numero} é par.");
+                        else
+                            Console.WriteLine($"O número {numero} é ímpar.");
+                    }
                     else
-                        Console.WriteLine("O número {0} é ímpar.", numero);
+                    {
+                        Console.WriteLine("Este programa exige a entrada de um valor inteiro. "
+                            + $"O valor digitado '{valor ?? "<null>"}' não é compatível.");
+                    }
 
                     Console.Write("Gostaria de executar o programa novamente (s/n)? ");
-                    resposta = char.Parse(Console.ReadLine());
+                    resposta = Console.ReadLine().ToLower();
+
+                    while (resposta != "s" && resposta != "n")
+                    {
+                        Console.Clear();
+                        Console.WriteLine($"Digite (s) para sim ou (n) para não. " +
+                            $"O valor '{resposta ?? "<null>"}' não é compatível.");
+                        Console.Write("Gostaria de executar o programa novamente (s/n)? ");
+                        resposta = Console.ReadLine().ToLower();
+                    }
                     Console.Clear();
                 }
                 Console.WriteLine("Obrigado por utilizar o programa!");
