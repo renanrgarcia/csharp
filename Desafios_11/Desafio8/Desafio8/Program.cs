4bcb0fe9496f650a4e75e17429e2c62bb147f11f
Renan Garcia <renanrgarcia@gmail.com>
2022-11-16 12:20:50 -0300

    Desafio 8 - Ajuste reutilização do sistema + TryParse

--- Desafios_11/Desafio8/Desafio8/Program.cs
+++ Desafios_11/Desafio8/Desafio8/Program.cs
@@ -12,15 +12,28 @@ namespace Desafio8
         {
             try
             {
-                char resposta = 's';
-                while (resposta != 'n')
+                string resposta = "s";
+                while (resposta != "n")
                 {
                     Console.WriteLine("Esse programa calcula o salário líquido dado desconto ao salário bruto.");
-                    Console.Write("Digite o salário bruto: R$ ");
-                    double salarioBruto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                    double salarioBruto = 0.0;
+                    do
+                    {
+                        Console.Write("Digite o salário bruto: R$ ");
+                        string sb = Console.ReadLine();
+
+                        Console.Clear();
+                        bool successo = double.TryParse(sb, NumberStyles.Number, CultureInfo.InvariantCulture, out salarioBruto);
+                        if (!successo || salarioBruto <= 0.0)
+                        {
+                            Console.WriteLine("Você deve digitar um valor real positivo não nulo. "
+                                + $"O valor digitado '{sb ?? "<null>"}' não é compatível.");
+                        }
+                    } while (salarioBruto <= 0.0);
+
                     double salarioLiquido = 0.0;
                     double desconto = 0.0;
-
                     if (salarioBruto < 2000.0)
                     {
                         desconto = 0.25;
@@ -32,11 +45,20 @@ namespace Desafio8
                         salarioLiquido = salarioBruto - salarioBruto * desconto;
                     }
 
-                    Console.WriteLine("O salário líquido é R$ " + salarioLiquido.ToString("F2", CultureInfo.InvariantCulture) 
+                    Console.WriteLine("O salário líquido é R$ " + salarioLiquido.ToString("F2", CultureInfo.InvariantCulture)
                         + ". Desconto: " + desconto + "%.");
 
                     Console.Write("Gostaria de executar o programa novamente (s/n)? ");
-                    resposta = char.Parse(Console.ReadLine());
+                    resposta = Console.ReadLine().ToLower();
+
+                    while (resposta != "s" && resposta != "n")
+                    {
+                        Console.Clear();
+                        Console.WriteLine($"Digite (s) para sim ou (n) para não. " +
+                            $"O valor '{resposta ?? "<null>"}' não é compatível.");
+                        Console.Write("Gostaria de executar o programa novamente (s/n)? ");
+                        resposta = Console.ReadLine().ToLower();
+                    }
                     Console.Clear();
                 }
                 Console.WriteLine("Obrigado por utilizar o programa!");
