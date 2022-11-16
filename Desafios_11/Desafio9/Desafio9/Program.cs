0ebc755d896a48804ef21a7d91134dd1dfcb3b3a
Renan Garcia <renanrgarcia@gmail.com>
2022-11-16 12:26:56 -0300

    Desafio 9 - Ajuste reutilização do sistema + TryParse

--- Desafios_11/Desafio9/Desafio9/Program.cs
+++ Desafios_11/Desafio9/Desafio9/Program.cs
@@ -12,12 +12,27 @@ namespace Desafio9
         {
             try
             {
-                char resposta = 's';
-                while (resposta != 'n')
+                string resposta = "s";
+                while (resposta != "n")
                 {
                     Console.WriteLine("Esse programa retorna o valor de venda de um produto e o lucro obtido.");
-                    Console.Write("Digite o valor de compra do produto: ");
-                    double precoCompra = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                    double precoCompra = 0.0;
+                    bool sucesso = false;
+                    do
+                    {
+                        Console.Write("Digite o valor de compra do produto: ");
+                        string pc = Console.ReadLine();
+
+                        Console.Clear();
+                        sucesso = double.TryParse(pc, NumberStyles.Number, CultureInfo.InvariantCulture, out precoCompra);
+                        if (!sucesso || precoCompra <= 0.0)
+                        {
+                            Console.WriteLine("Você deve digitar um valor real positivo não nulo. "
+                                + $"O valor digitado '{pc ?? "<null>"}' não é compatível.");
+                        }
+                    } while (!sucesso || precoCompra <= 0.0);
+                                        
                     double precoVenda = 0.0;
                     double lucro = 0.0;
 
@@ -38,7 +53,16 @@ namespace Desafio9
                         + ". A porcentagem de lucro foi: " + porcentagemLucro.ToString("F1", CultureInfo.InvariantCulture) + "%.");
 
                     Console.Write("Gostaria de executar o programa novamente (s/n)? ");
-                    resposta = char.Parse(Console.ReadLine());
+                    resposta = Console.ReadLine().ToLower();
+
+                    while (resposta != "s" && resposta != "n")
+                    {
+                        Console.Clear();
+                        Console.WriteLine($"Digite (s) para sim ou (n) para não. " +
+                            $"O valor '{resposta ?? "<null>"}' não é compatível.");
+                        Console.Write("Gostaria de executar o programa novamente (s/n)? ");
+                        resposta = Console.ReadLine().ToLower();
+                    }
                     Console.Clear();
                 }
                 Console.WriteLine("Obrigado por utilizar o programa!");
