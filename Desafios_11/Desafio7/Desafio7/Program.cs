0fdc76738f572786fd46d2647f7c5beedcb16cb7
Renan Garcia <renanrgarcia@gmail.com>
2022-11-16 12:14:39 -0300

    Desafio 7 - Ajuste reutilização do sistema + TryParse

--- Desafios_11/Desafio7/Desafio7/Program.cs
+++ Desafios_11/Desafio7/Desafio7/Program.cs
@@ -11,14 +11,42 @@ namespace Desafio7
         {
             try
             {
-                char resposta = 's';
-                while (resposta != 'n')
+                string resposta = "s";
+                while (resposta != "n")
                 {
                     Console.WriteLine("Esse programa determina o maior entre dois valores diferentes.");
-                    Console.Write("Informe os dois valores com espaço entre eles: ");
-                    string[] s = Console.ReadLine().Split(' ');
-                    double numero1 = double.Parse(s[0], CultureInfo.InvariantCulture);
-                    double numero2 = double.Parse(s[1], CultureInfo.InvariantCulture);
+
+                    double numero1 = 0.0;
+                    bool sucesso = false;
+                    do
+                    {
+                        Console.Write("Informe o primeiro valor real: ");
+                        string n1 = Console.ReadLine();
+
+                        Console.Clear();
+                        sucesso = double.TryParse(n1, NumberStyles.Number, CultureInfo.InvariantCulture, out numero1);
+                        if (!sucesso)
+                        {
+                            Console.WriteLine("Você deve digitar um valor real. "
+                                + $"O valor digitado '{n1 ?? "<null>"}' não é compatível.");
+                        }
+                    } while (!sucesso);
+
+                    double numero2 = 0.0;
+                    sucesso = false;
+                    do
+                    {
+                        Console.Write("Informe o segundo valor real: ");
+                        string n2 = Console.ReadLine();
+
+                        Console.Clear();
+                        sucesso = double.TryParse(n2, NumberStyles.Number, CultureInfo.InvariantCulture, out numero2);
+                        if (!sucesso)
+                        {
+                            Console.WriteLine("Você deve digitar um valor real. "
+                                + $"O valor digitado '{n2 ?? "<null>"}' não é compatível.");
+                        }
+                    } while (!sucesso);
 
                     if (numero1 == numero2)
                         Console.WriteLine($"Os números {numero1.ToString(CultureInfo.InvariantCulture)} e {numero2.ToString(CultureInfo.InvariantCulture)} são iguais.");
@@ -28,7 +56,16 @@ namespace Desafio7
                         Console.WriteLine($"O número {numero2.ToString(CultureInfo.InvariantCulture)} é maior que {numero1.ToString(CultureInfo.InvariantCulture)}.");
 
                     Console.Write("Gostaria de executar o programa novamente (s/n)? ");
-                    resposta = char.Parse(Console.ReadLine());
+                    resposta = Console.ReadLine().ToLower();
+
+                    while (resposta != "s" && resposta != "n")
+                    {
+                        Console.Clear();
+                        Console.WriteLine($"Digite (s) para sim ou (n) para não. " +
+                            $"O valor '{resposta ?? "<null>"}' não é compatível.");
+                        Console.Write("Gostaria de executar o programa novamente (s/n)? ");
+                        resposta = Console.ReadLine().ToLower();
+                    }
                     Console.Clear();
                 }
                 Console.WriteLine("Obrigado por utilizar o programa!");
